e048ea0e23cbc9ab1d84151882792f15b5fa0c13
xinhuang <xinhuang.abc@gmail.com>
2016-05-20 13:41:32 +0800

    add tagline to main page

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,10 +1,27 @@
 export class MainController {
-  constructor($rootScope, $scope, $http) {
+  constructor($rootScope, $scope, $http, $interval) {
     'ngInject';
 
     $rootScope.header = 'Life is short,';
+    $scope.title = 'Life is short, ';
+    $scope.tagline = 'eat more.';
     $http.get('assets/posts/list.json').success((list) => {
       $scope.blogs = list.blogs;
     });
+
+    let itl = 0;
+    const taglines = [
+      'eat more.',
+      'sleep more.',
+      'play more.',
+      'code more.',
+    ];
+    $interval(() => {
+      $scope.tagline = taglines[itl];
+      itl += 1;
+      if (itl >= taglines.length) {
+        itl = 0;
+      }
+    }, 3000);
   }
 }
