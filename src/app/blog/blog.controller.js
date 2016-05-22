0e809047f07393e42befcc968b7199106e812d29
xinhuang <xinhuang.abc@gmail.com>
2016-05-22 18:41:33 +0800

    set blog title as page title

--- src/app/blog/blog.controller.js
+++ src/app/blog/blog.controller.js
@@ -2,18 +2,22 @@ import path from 'path';
 import parser from '../components/blog-parser';
 
 export class BlogController {
-  constructor($rootScope, $scope, $stateParams, $http) {
+  constructor($rootScope, $stateParams, $http) {
     'ngInject';
 
+    $rootScope.header = "Loading...";
+
     const blogFileName = $stateParams['blogFile'];
-    $rootScope.header = blogFileName;
     const blogFile = path.join('/assets/posts/', blogFileName);
 
     $http.get(blogFile).success((data) => {
       const lines = data.split('\n');
       const post = parser.parse(blogFile, lines);
+
+      $rootScope.header = post.header.title;
+
       const content = `# ${post.header.title}\n${post.content.join('\n')}`;
-      $scope.data = content;
+      this.content = content;
     });
   }
 }
