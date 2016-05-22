d5cd1897fb2bca3351fdcd2d64bcd2f5bfe852fc
xinhuang <xinhuang.abc@gmail.com>
2016-05-22 18:48:23 +0800

    blog: add top link

--- src/app/blog/blog.controller.js
+++ src/app/blog/blog.controller.js
@@ -2,7 +2,7 @@ import path from 'path';
 import parser from '../components/blog-parser';
 
 export class BlogController {
-  constructor($rootScope, $stateParams, $http) {
+  constructor($rootScope, $stateParams, $http, $location, $anchorScroll) {
     'ngInject';
 
     $rootScope.header = "Loading...";
@@ -19,5 +19,13 @@ export class BlogController {
       const content = `# ${post.header.title}\n${post.content.join('\n')}`;
       this.content = content;
     });
+
+    this.scrollToTop = () => {
+      if ($location.hash() != 'top') {
+        $location.hash('top');
+      } else {
+        $anchorScroll();
+      }
+    };
   }
 }
