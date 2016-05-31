7f152f81fda82ab2c3bb3adafb363b8e38335095
xinhuang <xinhuang.abc@gmail.com>
2016-06-01 00:52:38 +0800

    [pages] temp solution for page header

--- src/app/page/page.controller.js
+++ src/app/page/page.controller.js
@@ -4,11 +4,11 @@ export class PageController {
   constructor($rootScope, $stateParams, $http, $location, $anchorScroll) {
     'ngInject';
 
-    $rootScope.header = "pages";
+    // TODO: fix header
+    $rootScope.header = "Resume - Xin Huang";
 
     const pageFileName = $stateParams['pageFile'];
     const pageFile = path.join('/assets/pages/', pageFileName);
-    console.log(pageFileName);
 
     $http.get(pageFile).success((data) => {
       this.content = data;
