957f4bb5b5cc7dc1e1f0d75c95d15eaa64c90695
xinhuang <xinhuang.abc@gmail.com>
2016-12-14 13:11:44 +0800

    [build] inject bower files using wiredep

--- gulp/build.js
+++ gulp/build.js
@@ -1,9 +1,24 @@
+import conf from './conf';
 import gulp from 'gulp';
+import path from 'path';
+import print from 'gulp-print';
+
+var wiredep = require('wiredep').stream;
+var $ = require('gulp-load-plugins')({
+  pattern: ['del']
+});
 
 gulp.task('clean', () => {
-
+    return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
 gulp.task('build', () => {
 
+});
+
+gulp.task('inject', () => {
+    return gulp.src([path.join(conf.paths.src, '**/*.html')])
+        .pipe(print())
+        .pipe(wiredep(conf.wiredep))
+        .pipe(gulp.dest(path.join(conf.paths.tmp, 'serve')));
 });
