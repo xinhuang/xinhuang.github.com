4d129baccc5ba9423a68179dd60bb0ebbcd8d80e
xinhuang <xinhuang.abc@gmail.com>
2019-03-30 11:08:49 -0400

    Reformat more files Add missing theme files Clean up .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -28,26 +28,4 @@
 					:recursive t
 					:publishing-function org-publish-attachment))))))
 
-;; org project
-;; (setq org-publish-project-alist
-;;       `(("org"
-;; 	 :components("org-pages"))
-;; 	("org-pages"
-;; 	 :base-directory ,project-dir
-;; 	 :base-extension "org"
-;; 	 :publishing-directory ,publish-dir
-;; 	 :recursive t
-;; 	 :publishing-function org-html-publish-to-html
-;; 	 :author "Xin Huang"
-;; 	 :headline-levels 4
-;; 	 :auto-preamble t
-;; 	 :auto-sitemap t
-;; 	 :completion-function coil-publish-complete-index
-;; 	 )))
-;;	("org-assets"
-;;	 :base-directory "."
-;;	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-;;	 :publishing-directory ,publish-dir
-;;	 :recursive t
-;;	 :publishing-function org-publish-attachment)
-;;	))
+
